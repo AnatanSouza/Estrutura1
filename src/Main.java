09d1fe0e497a0a3585800ba6cfbf0e1b157a29ad
Anatan <anatan.souza@gmaill.com>
2020-02-28 20:15:29 -0300

    Teste

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public int soma(int n){
+
+        int[] vetor = new int[100];
+        int numero;
+
+        for (int i = 0; i< 100; i++){
+            numero = i;
+            vetor[i] = numero;
+
+        }
+
+    }
+}
